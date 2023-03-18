8ca588624f442fcfd0bcf5ad4c9e18993c08a848
6310110699 <6310110699@psu.ac.th>
2023-03-18 19:28:36 +0700

    start

---
+++ src/config.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyACV9L8list5CgyxQbsOw8hBmNeSQm_l8c",
+  authDomain: "final-assign311.firebaseapp.com",
+  projectId: "final-assign311",
+  storageBucket: "final-assign311.appspot.com",
+  messagingSenderId: "337072089893",
+  appId: "1:337072089893:web:a25487516b2a3bc00248cc",
+  measurementId: "G-RE0CRDJX97"
+}
+export default config
